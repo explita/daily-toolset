813cea1e591e3391cf227d3ab0c72bf1c348ec04
MrAlao <ishomon@gmail.com>
2025-01-01 09:05:57 +0100

    patch useDisclosure hook

--- src/hooks/useDisclosure.ts
+++ src/hooks/useDisclosure.ts
@@ -1,5 +1,11 @@
 import { useState } from "react";
 
+/**
+ * A custom React hook that manages the open/closed state of a component.
+ *
+ * @param defaultOpen - Initial open state (default is false).
+ * @returns A tuple containing the current open state and an object with methods to open, close, and toggle the state.
+ */
 export function useDisclosure(defaultOpen = false) {
   const [open, setOpen] = useState(defaultOpen);
 
@@ -10,5 +16,5 @@ export function useDisclosure(defaultOpen = false) {
       close: () => setOpen(false),
       toggle: () => setOpen(!open),
     },
-  ];
+  ] as const;
 }
